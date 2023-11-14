ab1b400e31867951a6dd9ed8c0c0a8869505f30a
Ho Wun Mok <hmok11@my.centennialcollege.ca>
2023-11-13 22:04:44 -0500

    env upload url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  // baseURL: "https://itdg-ng-questionnaire-backend.onrender.com/"
+  baseURL: 'https://itdg-ng-questionnaire-backend.onrender.com/',
 };
